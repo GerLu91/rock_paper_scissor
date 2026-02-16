b151ed57e724268180b56cf0cc1e98ea39977944
GerLu91 <luigigerstner@gmail.com>
2026-02-16 19:00:35 +0100

    Implemented Pseudo Code for .js

---
+++ javascript.js
@@ -0,0 +1,39 @@
+/*
+getComputerChoice
+
+- Random number generated means either P, R or S
+- Save Choice in var
+*/
+
+
+/* 
+getHumanChoice
+
+- Show human 3 options (R, P or S)
+- Get Input from human
+- save in variable
+*/
+
+/* 
+- create two variables humanScore and computerScore
+*/
+
+/* 
+playRound
+
+- Take Human and Computer choices as input
+- compare their choices
+- calculate who won
+- print out what was chosen this round and who won
+- increment human or computerScore by one for each victory
+
+
+*/
+
+/* 
+playGame
+
+- after each round compare the amount of victories for each party
+- repeat until first player has 5 victories to win the game
+*/
+
